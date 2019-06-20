90a5e96ca99240dbc92f0f281cd3995d3944f119
rimever <mot_siva_ordin@hotmail.com>
2019-06-20 22:50:35 +0900

    デフォルトのテンプレートにて作成。

---
+++ Simple3DGameDX/Simple3DGameDX/Simple3DGameDXMain.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+#include "Content\SampleFpsTextRenderer.h"
+
+// Direct2D および 3D コンテンツを画面上でレンダリングします。
+namespace Simple3DGameDX
+{
+	class Simple3DGameDXMain : public DX::IDeviceNotify
+	{
+	public:
+		Simple3DGameDXMain(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~Simple3DGameDXMain();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// デバイス リソースへのキャッシュされたポインター。
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: これを独自のコンテンツ レンダラーで置き換えます。
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+		std::unique_ptr<SampleFpsTextRenderer> m_fpsTextRenderer;
+
+		// ループ タイマーをレンダリングしています。
+		DX::StepTimer m_timer;
+	};
+}
